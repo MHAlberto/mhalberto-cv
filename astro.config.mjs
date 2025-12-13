70c027ad395957a3fc6d6708dc886bf38db6f97f
MHAlberto <mayiitoo_alberto@hotmail.com>
2025-12-13 09:11:58 -0600

    bug links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://MHAlberto.github.io',
-    base: 'mhalberto-cv',
+  site: 'https://mhalberto.github.io',
+  base: '/mhalberto-cv/',
 });
-
